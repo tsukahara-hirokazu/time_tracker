b6b2bcedb6990fc63e52e2138dbdfe2e0793c972
htsukahara <tsukahara.hirokazu@ajax-labo.com>
2020-02-20 14:37:32 +0900

    102_Creating a reusable SigiInButton

---
+++ lib/app/sign_in/sign_in_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:time_tracker/common_widget/custom_raised_button.dart';
+
+class SignInButton extends CutomRaisedButton {
+  SignInButton({
+    String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  }) : super(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 15.0,
+            ),
+          ),
+          color: color,
+          onPressed: onPressed,
+        );
+}

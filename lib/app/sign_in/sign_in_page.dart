be9cfaf55e4c44f10bd99aec60dc67153aae958a
htsukahara <tsukahara.hirokazu@ajax-labo.com>
2020-02-20 13:56:22 +0900

    95_Adding the first button

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            "Sign In",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          RaisedButton(
+            child: Text("Sign in with Google"),
+            onPressed: () {
+              print("button pressed");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}

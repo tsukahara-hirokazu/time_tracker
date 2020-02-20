90fab6a11488934bd7c83d30f11e5b3bf8d0e982
htsukahara <tsukahara.hirokazu@ajax-labo.com>
2020-02-20 15:39:35 +0900

    104_Making the button height configurable

--- lib/common_widget/custom_raised_button.dart
+++ lib/common_widget/custom_raised_button.dart
@@ -4,6 +4,7 @@ class CutomRaisedButton extends StatelessWidget {
   final Widget child;
   final Color color;
   final double borderRadius;
+  final double height;
   final VoidCallback onPressed;
 
   CutomRaisedButton({
@@ -11,19 +12,23 @@ class CutomRaisedButton extends StatelessWidget {
     this.color,
     this.borderRadius: 2.0,
     this.onPressed,
+    this.height: 50.0,
   });
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: child,
-      color: color,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(borderRadius),
+    return SizedBox(
+      height: height,
+      child: RaisedButton(
+        child: child,
+        color: color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(borderRadius),
+          ),
         ),
+        onPressed: onPressed,
       ),
-      onPressed: onPressed,
     );
   }
 }
